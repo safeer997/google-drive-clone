ee1c9e277f3fbadd78322fabaa94ca51e5c090cd
Safeer <safeeralam997@gmail.com>
2024-06-23 04:44:33 +0530

    add sidebar component

--- drive-clone/src/App.jsx
+++ drive-clone/src/App.jsx
@@ -3,13 +3,14 @@ import Files from "./components/Files";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
-
 function App() {
   return (
     <>
-      <Header></Header>
-      <Sidebar></Sidebar>
-      <Files></Files>
+      <Header />
+      <div className="app-main">
+        <Sidebar />
+        <Files />
+      </div>
     </>
   );
 }
--- drive-clone/src/components/Sidebar.jsx
+++ drive-clone/src/components/Sidebar.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import HomeIcon from "@mui/icons-material/Home";
+import PhonelinkIcon from "@mui/icons-material/Phonelink";
+import SupervisorAccountOutlinedIcon from "@mui/icons-material/SupervisorAccountOutlined";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import ReportGmailerrorredOutlinedIcon from "@mui/icons-material/ReportGmailerrorredOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import WbCloudyOutlinedIcon from "@mui/icons-material/WbCloudyOutlined";
+import "../css/Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="sidebar">
+      <div className="sidebar_button">
+        <button>
+          <AddIcon /> <span>New</span>
+        </button>
+      </div>
+      <div className="options">
+        <div className="single-option">
+          <HomeIcon />
+          <span>Home</span>
+        </div>
+
+        <div className="single-option">
+          <PhonelinkIcon />
+          <span>Computers</span>
+        </div>
+
+        <div className="single-option">
+          <SupervisorAccountOutlinedIcon />
+          <span>Shared with me</span>
+        </div>
+
+        <div className="single-option">
+          <ScheduleIcon />
+          <span>Recent</span>
+        </div>
+
+        <div className="single-option">
+          <StarBorderOutlinedIcon />
+          <span>Starred</span>
+        </div>
+
+        <div className="single-option">
+          <ReportGmailerrorredOutlinedIcon />
+          <span>Spam</span>
+        </div>
+
+        <div className="single-option">
+          <DeleteOutlineOutlinedIcon />
+          <span>Trash</span>
+        </div>
+
+        <div className="single-option">
+          <WbCloudyOutlinedIcon />
+          <span>Storage</span>
+        </div>
+
+        <div className="progress-bar">
+          <progress size="tiny" value="50" max="100" />
+          <span>350.6 MB of 15 GB used</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
