569d32c494deeffadf8e1861c911cc9594b47450
Safeer <safeeralam997@gmail.com>
2024-06-23 00:41:05 +0530

    folder setup

---
+++ drive-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Files from "./components/Files";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <Files></Files>
+    </>
+  );
+}
+
+export default App;
