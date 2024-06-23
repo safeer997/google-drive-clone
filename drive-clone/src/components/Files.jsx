52df59ce2765c16e29b73fec54863aca0c51b302
Safeer <safeeralam997@gmail.com>
2024-06-23 17:16:29 +0530

    add files component

--- drive-clone/src/components/Files.jsx
+++ drive-clone/src/components/Files.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import "../css/Files.css";
+import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 function Files() {
   return (
-    <div>Files</div>
-  )
+    <>
+      <div className="files">
+        <div className="data_list">
+          <div className="detailsrow detailsrow1">
+            <p>
+              <b className="icon">
+                Name
+                <span>
+                  <ArrowUpwardOutlinedIcon />
+                </span>
+              </b>
+            </p>
+            <p>
+              <b className="owner">Owner</b>
+            </p>
+            <p>
+              <b>Last modified</b>
+            </p>
+            <p>
+              <b>Size</b>
+            </p>
+          </div>
+          <div className="detailsrow">
+            <p>
+              <DriveFolderUploadOutlinedIcon className="folder-icon" />
+              Folder name
+            </p>
+            <p>Me</p>
+            <p>23 Jun, 2024</p>
+            <p>99 kb</p>
+          </div>
+          <div className="detailsrow">
+            <p>
+              <DriveFolderUploadOutlinedIcon className="folder-icon" />
+              Folder name
+            </p>
+            <p>Me</p>
+            <p>23 Jun, 2024</p>
+            <p>99 kb</p>
+          </div>
+          <div className="detailsrow">
+            <p>
+              <DriveFolderUploadOutlinedIcon className="folder-icon" />
+              Folder name
+            </p>
+            <p>Me</p>
+            <p>23 Jun, 2024</p>
+            <p>99 kb</p>
+          </div>
+          <div className="detailsrow">
+            <p>
+              <DriveFolderUploadOutlinedIcon className="folder-icon" />
+              Folder name
+            </p>
+            <p>Me</p>
+            <p>23 Jun, 2024</p>
+            <p>99 kb</p>
+          </div>
+          <div className="detailsrow">
+            <p>
+              <DriveFolderUploadOutlinedIcon className="folder-icon" />
+              Folder name
+            </p>
+            <p>Me</p>
+            <p>23 Jun, 2024</p>
+            <p>99 kb</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Files
+export default Files;
