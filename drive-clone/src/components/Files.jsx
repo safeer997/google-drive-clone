5dd5776fdb83ccddd9a6b2a5dc1faa61ef115e6f
Safeer <safeeralam997@gmail.com>
2024-06-27 04:26:16 +0530

    styled icons

--- drive-clone/src/components/Files.jsx
+++ drive-clone/src/components/Files.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import "../css/Files.css";
-import { format } from 'date-fns';
+import { format } from "date-fns";
+
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import DriveFileRenameOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
 
 function Files({ files, deleteFile, renameFile }) {
   const [renameFileId, setRenameFileId] = useState(null);
@@ -21,42 +24,53 @@ function Files({ files, deleteFile, renameFile }) {
   // Function to render the list of files
   const renderFiles = () => {
     return files.map((file) => (
-      <div className="detailsrow" key={file._id}>
+      <div key={file._id}>
         {renameFileId === file._id ? (
           <>
-            <input
+            <input 
               type="text"
               value={newFileName}
               onChange={(e) => setNewFileName(e.target.value)}
             />
-            <button onClick={() => handleRename(file._id)}>Save</button>
-            <button onClick={() => setRenameFileId(null)}>Cancel</button>
+            <button className="save-icon" onClick={() => handleRename(file._id)}>Save</button>
+            <button className="cancel-icon" onClick={() => setRenameFileId(null)}>Cancel</button>
           </>
         ) : (
           <>
-            <p>
-              <InsertDriveFileIcon className="file-icon" />
-              {file.name}
-            </p>
-            <p>Me</p>
-            <p>{format(new Date(file.updatedAt), 'yyyy-MM-dd')}</p>
-            <p>{(file.size / 1024).toFixed(2)} kb</p>
-            <button onClick={() => deleteFile(file._id)}>Delete</button>
-            <button onClick={() => startRenaming(file._id, file.name)}>Rename</button>
+            <div className="detailsrow">
+              <p>
+                <InsertDriveFileIcon className="file-icon" />
+                {file.name}
+              </p>
+              <p>Me</p>
+              <p>{format(new Date(file.updatedAt), "yyyy-MM-dd")}</p>
+              <p>{(file.size / 1024).toFixed(2)} kb</p>
+            </div>
+
+            <div className="rename-delete-icons-div">
+             
+              <button className="delete-icon" onClick={() => deleteFile(file._id)}>
+                <DeleteForeverIcon style={{ fontSize: "14px" }} />
+              </button>
+              <button className="rename-icon" onClick={() => startRenaming(file._id, file.name)}>
+                <DriveFileRenameOutlineOutlinedIcon
+                  style={{ fontSize: "14px" }}
+                />
+              </button>
+            </div>
+            <div className="hr-line"></div>
           </>
         )}
       </div>
     ));
   };
 
-  
-
   return (
     <>
       <div className="files">
         <div className="add_folder_file"></div>
         <div className="data_list">
-          <div className="detailsrow detailsrow1">
+          <div className="detailsrow hr-line">
             <p>
               <b className="icon">
                 Name
